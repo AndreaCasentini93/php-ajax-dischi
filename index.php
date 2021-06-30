1bc2a2d38142eb87f361a4935423a04c55b585b8
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-30 15:49:58 +0200

    Discs Base

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<?php
-    /*
-    Create un array di 10 dischi (con, per ciascun disco, informazioni su titolo, autore, anno, genere e percorso della copertina) in un file database.php.
-    Importate questo file in un file index.php nel quale stampare, lato server, una card per ogni disco.
-
-    Bonus:
-    Una volta completata la parte PHP dell'esercizio, se volete, potete creare un layout (HTML/CSS) diverso da quello definito qualche giorno fa per l'analogo esercizio fatto con i componenti Vue.
-    */
-
-    require_once __DIR__ . '/partials/database/database.php';
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
