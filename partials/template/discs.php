8c3c6b82bd1b393fc818b4e4133a6c69d62d64cc
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-30 16:20:11 +0200

    Final Style Refactoring

--- partials/template/discs.php
+++ partials/template/discs.php
@@ -1,12 +1,14 @@
+<!-- REQUIRE DATABASE -->
 <?php require_once __DIR__ . '/../database/database.php'; ?>
 
+<!-- CARDS -->
 <?php foreach ($discs as $disc) { ?>
-    <div class="disc">
+    <a href="<?= '#'; ?>" class="disc">
         <div class="image_box">
             <i class="far fa-play-circle"></i>
             <img src="<?= $disc['cover']; ?>" alt="<?= $disc['title']; ?>">
         </div>
         <h2><?= $disc['title']; ?></h2>
         <h4><?= $disc['author']; ?></h4>
-    </div>          
+    </a>          
 <?php } ?>
