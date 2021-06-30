4c06e3b2150144a07cb8cb2253c9c28d5906a67f
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-30 14:18:01 +0200

    Refactoring

--- index.php
+++ index.php
@@ -9,6 +9,27 @@
 
     require __DIR__ . '/database/database.php';
 
-    var_dump($discs)
+    var_dump($discs);
     
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- FONTS GOOGLE -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <!-- STYLE CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- TITLE -->
+        <title>Listening is Everything - Spotify</title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
