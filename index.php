beaf7215a43b1173395095b3342e2097863b1cce
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-30 13:13:14 +0200

    File index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+    Create un array di 10 dischi (con, per ciascun disco, informazioni su titolo, autore, anno, genere e percorso della copertina) in un file database.php.
+    Importate questo file in un file index.php nel quale stampare, lato server, una card per ogni disco.
+
+    Bonus:
+    Una volta completata la parte PHP dell'esercizio, se volete, potete creare un layout (HTML/CSS) diverso da quello definito qualche giorno fa per l'analogo esercizio fatto con i componenti Vue.
+    */
+
+    
+?>
