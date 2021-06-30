8b171fe6a20bc6a4e36976b3afb48fed11406090
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-30 14:02:01 +0200

    Database

--- database/database.php
+++ database/database.php
@@ -1,11 +1,73 @@
 <?php
-
     $discs = [
         [
-            "title" => "a",
-            "author" => "a",
-            "year" => "a",
-            "genre" => "a",
-            "cover" => "a"
+            "title" => "Bohemian Rhapsody",
+            "author" => "Queen",
+            "year" => "31-10-1985",
+            "genre" => "Rock",
+            "cover" => "https://lh3.googleusercontent.com/proxy/XnXODVDxPamzkdbhvc5e2KFsD9KMXmn6JeyQFUA5s84Ht1dti3S7a6lNIYuEN5DvstDMmrUxGnBQZcx3J87VzrwTooA8iECLiPxphKiDa5LQqNwQDSXcR2RXsKJFLGjpngr87dc6yKIyyjq_CAaz8bj4R5Ko"
         ],
+        [
+            "title" => "The Miracle",
+            "author" => "Queen",
+            "year" => "22-05-1989",
+            "genre" => "Rock",
+            "cover" => "https://lh3.googleusercontent.com/proxy/kuREb95mPHKDOKX50l0F2iCvE7hLMFGFFbm1IJ6ArJy6RlhRJfPX7JB5OO0U_sOIcgHezUmmORXButHXs7e1JBTRchxd5aaCnq22ggS0VDA57opxMA3hMvg2sQ"
+        ],
+        [
+            "title" => "Nursery Crime",
+            "author" => "Genesis",
+            "year" => "12-11-1971",
+            "genre" => "Rock",
+            "cover" => "https://legendarycover.it/wp-content/uploads/2019/10/nursery-cryme-genesis.jpg"
+        ],
+        [
+            "title" => "Chromatica",
+            "author" => "Lady Gaga",
+            "year" => "29-05-2020",
+            "genre" => "Pop",
+            "cover" => "https://www.radiolombardia.it/wp-content/uploads/2020/04/Lady-Gaga_cover-album_Chromatica_bassa.jpg"
+        ],
+        [
+            "title" => "Bad",
+            "author" => "Michael Jackson",
+            "year" => "31-08-1987",
+            "genre" => "Pop",
+            "cover" => "https://www.artovercovers.com/wp-content/uploads/2014/12/michael-jackson-bad.jpg"
+        ],
+        [
+            "title" => "Killers",
+            "author" => "Iron Maiden",
+            "year" => "02-02-1981",
+            "genre" => "Metal",
+            "cover" => "https://www.artovercovers.com/wp-content/uploads/2020/12/R-1056834-1322400752.jpeg.jpg"
+        ],
+        [
+            "title" => "Life After Death",
+            "author" => "Iron Maiden",
+            "year" => "14-10-1985",
+            "genre" => "Metal",
+            "cover" => "https://lh3.googleusercontent.com/proxy/LQXACk1hiL3NhfecGLXh840qsS5TC-tpu7947-1yB95rxDfnwzrPo5oY4NMD-By8KklzafeaUl7NAUr-FWP_aFKp6V57t2vw7H6N7CoGGYxPVr9IZyZmChIlz474TMi3r5IAITRE"
+        ],
+        [
+            "title" => "Lover",
+            "author" => "Taylor Swift",
+            "year" => "23-08-2019",
+            "genre" => "Country",
+            "cover" => "https://www.optimagazine.com/wp-content/uploads/2019/06/lover-taylor-swift-555x555.jpg"
+        ],
+        [
+            "title" => "Out Among The Stars",
+            "author" => "Johnny Cash",
+            "year" => "25-03-2014",
+            "genre" => "Country",
+            "cover" => "https://lh3.googleusercontent.com/proxy/n4eeR-ZhJ7QBCmlenhKA5KfioYUuvX4gSfJ1BNh5kCcoDWgVJ71c9_BKTQUYU1RET5Ig_VTmlA2hy0G7d1vX2fil4k117ZwIOXeGsuFE_gL2_BMpyYDruHs8E8OQy0YRyRFGLFs8BJqz6WX1H6E"
+        ],
+        [
+            "title" => "Slowhand",
+            "author" => "Eric Clapton",
+            "year" => "25-11-1977",
+            "genre" => "Blues",
+            "cover" => "https://lh3.googleusercontent.com/proxy/BgE37KHhe7E1a5ht5uLnVkqRokLScKN3_3gFjbuRpbrpipUay5Moxug_zIcv7Dv0GhAeB6wWwTxtvHHBcKlQySOkGqV_xxMrDerTafPcAN6a6OhvuXtout5sWVcaZDc"
+        ]
     ];
